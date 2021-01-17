b3ee816b1a5092cf489e4542134688cedac4eca3
Mehran Zahiri <zahiri@arad-itc.org>
2021-01-17 16:58:56 +0330

    Revert "initial commit"
    This reverts commit cf679c89b65b8b578080dc31df532319346a06b5.

--- src/main/java/com/araditc/uploader/constants/UploadHistoryTypes.java
+++
@@ -1,8 +0,0 @@
-package com.araditc.uploader.constants;
-
-public class UploadHistoryTypes {
-    public static final int STARTED = 1001;
-    public static final int IN_PROGRESS = 1002;
-    public static final int NOT_COMPLETED = 1003;
-    public static final int IS_COMPLETED = 1004;
-}
